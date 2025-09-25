d9566710f44cdf6e0dacdae9b770559b4108ce81
BenardNyali456 <palmerbt456@gmail.com>
2025-09-25 15:17:07 +0300

    Add files via upload

---
+++ Semester Units display.c
@@ -0,0 +1,21 @@
+//simple c program
+
+/*
+Name:Benard Nyali 
+Reg:CT100/G/26119/25
+Description:Display Semester Units
+*/
+
+//main function 
+int main() {
+    printf("Units for this Semester \n");
+    printf("-Mathematics for Science \n");
+    printf("-Analogue Electronics \n");
+    printf("-Introduction to Computer Applications \n");
+    printf("-Computer systems and organisations \n");
+    printf("-Essentials of Accounting \n");
+    printf("-Fundamentals of ICT \n");
+    printf("-Introduction to Programming and Problem Solving \n");
+    printf("-Communication Skills and Information Literacy \n");
+    return 0;
+    }
